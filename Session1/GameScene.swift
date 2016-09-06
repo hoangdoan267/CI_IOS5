34604c3a20808cf9c33d39b475fb8e4eff72f2f1
Hoang Doan <hoang.doan@outlook.com>
2016-09-06 19:39:10 +0700

    Session 1

--- Session1/GameScene.swift
+++ Session1/GameScene.swift
@@ -9,36 +9,80 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var plane: SKSpriteNode!
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        addBackground()
+        addPlane()
         
-        self.addChild(myLabel)
+    }
+    
+    func addBackground() {
+        //1
+        let background = SKSpriteNode(imageNamed: "background.png")
+        //2
+        background.anchorPoint = CGPointZero
+        background.position = CGPointZero
+        //3
+        addChild(background)
+        
+    }
+    
+    func addPlane() {
+        //1
+        plane = SKSpriteNode(imageNamed: "plane3.png")
+        plane.position = CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2)
+        
+        
+        addChild(plane)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+        /* Called when a touch begins */
+        print("touchBegan")
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        print("touchesMoved")
+        print("touches: \(touches.count)")
+        if let touch = touches.first {
+            let currentTouch = touch.locationInNode(self)
+            let previousTouch = touch.previousLocationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+//            let movementVector = currentTouch.subtract(previousTouch)
+//            
+//            let newPosition = movementVector.add(plane.position)
+           
+            plane.position = currentTouch.subtract(previousTouch).add(plane.position)
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            let multiExmaple = plane.position.multiply(2)
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
             
-            sprite.runAction(SKAction.repeatActionForever(action))
             
-            self.addChild(sprite)
+            if(plane.position.x < 0) {
+                plane.position.x = 0
+            }
+            
+            if (plane.position.x > self.frame.width) {
+                plane.position.x = self.frame.width
+            }
+            
+            if (plane.position.y - plane.size.height/2 < 0 || plane.position.y - plane.size.height/2 == 0  ) {
+                plane.position.y = plane.size.height/2
+            }
+            
+            if (plane.position.y + plane.size.height/2 > self.frame.height) {
+                plane.position.y = self.frame.height - plane.size.height/2
+            }
+            
+            print("Vi tri may bay: \(plane.position)")
+            print("Diem sau khi multiply: \(multiExmaple)")
+            print("Distance: \(currentTouch.distance(previousTouch))")
+            
         }
     }
-   
+    
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
--- Session1/GameViewController.swift
+++ Session1/GameViewController.swift
@@ -13,21 +13,33 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //1 cast view to skView
+        print("1")
+        let skView = self.view as! SKView
+        
+        //2 Create Game Scene
+        print("2")
+        let gameScene = GameScene(size: skView.frame.size)
+        
+        //3 Present Game Scene
+        print("3")
+        skView.presentScene(gameScene)
 
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+//        if let scene = GameScene(fileNamed:"GameScene") {
+//            // Configure the view.
+//            let skView = self.view as! SKView
+//            skView.showsFPS = true
+//            skView.showsNodeCount = true
+//            
+//            /* Sprite Kit applies additional optimizations to improve rendering performance */
+//            skView.ignoresSiblingOrder = true
+//            
+//            /* Set the scale mode to scale to fit the window */
+//            scene.scaleMode = .AspectFill
+//            
+//            skView.presentScene(scene)
+//        }
     }
 
     override func shouldAutorotate() -> Bool {
